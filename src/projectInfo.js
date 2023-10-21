2b9a6a607462e0b0bdfa71aa45d64fa2248b7597
marieverdonck9 <marieverdonck9@gmail.com>
2023-10-21 23:36:51 +0200

    initial setup

--- src/projectInfo.js
+++ src/projectInfo.js
@@ -1,22 +1,48 @@
 export const projectInfo = [
   {
-    name: "Project 1",
-    date: "June 2023",
-    link: "",
-    skills: ["Graphs", "Clustering", "Web development"],
-    description: "Description",
+    name: "Applying Systems Biology to Give Lone Genes a Meaning",
+    date: "August-December 2021",
+    link: "???",
+    skills: ["Cytoscape", "Overrepresentation analysis", "Network Analysis"],
+    description:
+      "In this project, I delved into Systems Biology, exploring 25 specific genes. Using statistical tools and databases like BiNGO, Reactome, Panther, UniProtKB, and more, I constructed a network in Cytoscape focused on the human immune system. Comparing gene expression in colorectal cancer and normal tissue, I found lower expression of apoptotic genes and the overexpression of SOCS1 and SMAD3, promoting tumor formation. This work highlighted the dynamic nature of biological systems and their potential in cancer treatment.",
   },
   {
-    name: "Project 2",
-    date: "December 2021",
-    link: "https://github.com/sjoerdvink99/apml_reinforcement_learning/blob/main/Assignment_4_for_APML2022.pdf",
-    skills: ["Python", "OpenAI", "Numpy"],
+    name: "Antimicrobial production in nontoxigenic Clostridium botulinum",
+    date: "February - June 2022",
+    link: "???",
+    skills: [
+      "Genomic Analysis",
+      "Microbiological Techniques",
+      "Microscopy Techniques",
+    ],
+    description:
+      "In my Bachelor’s thesis, I explored the antimicrobial potential of three nontoxigenic Clostridium botulinum strains (1985, HA1, and ZO42). I identified bacteriocins, proteinaceous compounds with antibacterial properties, within these strains through whole genome sequence analysis. The study revealed highly potent compounds with distinct modes of action, shedding light on their potential applications as alternative antimicrobial agents.",
+  },
+  {
+    name: "Characterizing the fitness landscape of adaptation using high-throughput genome editing",
+    date: "September - June 2023",
+    link: "???",
+    skills: [
+      "CRISPR/Cas9 genome engineering",
+      "Next-generation sequencing",
+      "Bacterial and yeast transformation",
+      "Molecular cloning",
+    ],
+    description:
+      "In my Master’s thesis, I delve into genetic mutations and their impact on adaptation in S. cerevisiae. My main goal is to unravel the intricacies of the fitness landscape experienced by strains during the adaptation process. To achieve this, I am developing a specialized pipeline to introduce several mutations in yeast cells. By dissecting the genetic components that drive adaptation, I aim to gain a deeper understanding of how organisms evolve in response to changing environments, ultimately contributing valuable insights to the broader field of evolutionary biology.",
+  },
+  {
+    name: "Project 3",
+    date: "November 2021",
+    link: "???",
+    skills: ["Python", "Classification", "Clustering", "SK-Learn"],
     description: "Description",
   },
   {
     name: "Project 3",
     date: "November 2021",
-    link: "",
+    link: "???",
     skills: ["Python", "Classification", "Clustering", "SK-Learn"],
     description: "Description",
   },
