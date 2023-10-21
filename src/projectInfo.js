d0fdba8845e5489c9f24fe136614061f1f8fb46c
marieverdonck9 <148657300+marieverdonck9@users.noreply.github.com>
2023-10-22 00:28:47 +0200

    Update projectInfo.js

--- src/projectInfo.js
+++ src/projectInfo.js
@@ -1,25 +1,5 @@
 export const projectInfo = [
-  {
-    name: "Applying Systems Biology to Give Lone Genes a Meaning",
-    date: "August-December 2021",
-    link: "???",
-    skills: ["Cytoscape", "Overrepresentation analysis", "Network Analysis"],
-    description:
-      "In this project, I delved into Systems Biology, exploring 25 specific genes. Using statistical tools and databases like BiNGO, Reactome, Panther, UniProtKB, and more, I constructed a network in Cytoscape focused on the human immune system. Comparing gene expression in colorectal cancer and normal tissue, I found lower expression of apoptotic genes and the overexpression of SOCS1 and SMAD3, promoting tumor formation. This work highlighted the dynamic nature of biological systems and their potential in cancer treatment.",
-  },
-  {
-    name: "Antimicrobial production in nontoxigenic Clostridium botulinum",
-    date: "February - June 2022",
-    link: "???",
-    skills: [
-      "Genomic Analysis",
-      "Microbiological Techniques",
-      "Microscopy Techniques",
-    ],
-    description:
-      "In my Bachelor’s thesis, I explored the antimicrobial potential of three nontoxigenic Clostridium botulinum strains (1985, HA1, and ZO42). I identified bacteriocins, proteinaceous compounds with antibacterial properties, within these strains through whole genome sequence analysis. The study revealed highly potent compounds with distinct modes of action, shedding light on their potential applications as alternative antimicrobial agents.",
-  },
-  {
+ {
     name: "Characterizing the fitness landscape of adaptation using high-throughput genome editing",
     date: "September - June 2023",
     link: "???",
@@ -33,17 +13,51 @@ export const projectInfo = [
       "In my Master’s thesis, I delve into genetic mutations and their impact on adaptation in S. cerevisiae. My main goal is to unravel the intricacies of the fitness landscape experienced by strains during the adaptation process. To achieve this, I am developing a specialized pipeline to introduce several mutations in yeast cells. By dissecting the genetic components that drive adaptation, I aim to gain a deeper understanding of how organisms evolve in response to changing environments, ultimately contributing valuable insights to the broader field of evolutionary biology.",
   },
   {
+    name: "3D analysis of the Fv structure of the IgE SPE-7 in complex with acenaphthenequinone",
+    date: "May 2023",
+    link: "???",
+    skills: ["Structural Biology", "PyMOL"],
+    description: "Structural analysis of an antibody-antigen complex using PyMOL. Gained insights into the antigen-binding sites by identifying complementarity-determining regions within the antibody",
+  },
+  {
      name: "Histology of pig and chicken lymphoid organs",
     date: "May 2023",
     link: "???",
     skills: ["Histological Analysis", "Microscopy Techniques"],
-    description: "In this project, I delved into histology as a vital component of my immunology practicals. My primary focus was on the examination of histological sections from pig and chicken lymphoid organs, with an emphasis on understanding how these samples were prepared. This work aimed to deepen my practical knowledge of immunology, specifically in the context of histological analysis and the techniques involved in sample preparation.",
+    description: "Delved into histology as a vital component of my immunology practicals. Primary focus was on the examination of histological sections from pig and chicken lymphoid organs, with an emphasis on understanding how these samples were prepared.",
+  },
+   {
+    name: "Antimicrobial production in nontoxigenic Clostridium botulinum",
+    date: "February - June 2022",
+    link: "???",
+    skills: [
+      "Genomic Analysis",
+      "Microbiological Techniques",
+      "Microscopy Techniques",
+    ],
+    description:
+      "In my Bachelor’s thesis, I explored the antimicrobial potential of three nontoxigenic Clostridium botulinum strains (1985, HA1, and ZO42). I identified bacteriocins, proteinaceous compounds with antibacterial properties, within these strains through whole genome sequence analysis. The study revealed highly potent compounds with distinct modes of action, shedding light on their potential applications as alternative antimicrobial agents.",
   },
   {
-    name: "Project 3",
+    name: "Applying Systems Biology to Give Lone Genes a Meaning",
+    date: "August-December 2021",
+    link: "???",
+    skills: ["Cytoscape", "Overrepresentation analysis", "Network Analysis"],
+    description:
+      "In this project, I delved into Systems Biology, exploring 25 specific genes. Using statistical tools and databases like BiNGO, Reactome, Panther, UniProtKB, and more, I constructed a network in Cytoscape focused on the human immune system. Comparing gene expression in colorectal cancer and normal tissue, I found lower expression of apoptotic genes and the overexpression of SOCS1 and SMAD3, promoting tumor formation. This work highlighted the dynamic nature of biological systems and their potential in cancer treatment.",
+  },
+   {
+    name: "Analyzing how transposon Tn3E.F. influences antibiotic resistance in E. coli strains S17.1 and C2110",
     date: "November 2021",
     link: "???",
-    skills: ["Python", "Classification", "Clustering", "SK-Learn"],
-    description: "Description",
+    skills: ["Genetic Manipulation", "Molecular Biology Techniques"],
+    description: "Used transposons to introduce mutations in E. coli to alter the resistance characteristics of the target plasmid. The goal was to investigate the mutagenic capabilities of transposons and their broader scientific relevance.",
+  },
+ {
+    name: "Using the QuickChange method to introduce mutations into algL whilst adding a restriction site for BpmI",
+    date: "November 2021",
+    link: "???",
+    skills: ["Genetic Manipulation", "Molecular Biology Techniques"],
+    description: "Conducted site-directed mutagenesis to introduce specific genetic mutations in Pseudomonas fluorescens. The goal was to investigate genetic modification techniques, contributing to a deeper understanding of precise genetic alterations and their broader applications.",
   },
 ];
