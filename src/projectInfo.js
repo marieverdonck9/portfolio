0522f7d710e40c1acd3649466f4d708226dacda9
marieverdonck9 <148657300+marieverdonck9@users.noreply.github.com>
2023-10-22 11:33:39 +0200

    Update projectInfo.js

--- src/projectInfo.js
+++ src/projectInfo.js
@@ -10,15 +10,15 @@ export const projectInfo = [
       "Molecular cloning",
     ],
     description:
-      "In my Master’s thesis, I delve into genetic mutations and their impact on adaptation in S. cerevisiae. My main goal is to unravel the intricacies of the fitness landscape experienced by strains during the adaptation process. To achieve this, I am developing a specialized pipeline to introduce several mutations in yeast cells. By dissecting the genetic components that drive adaptation, I aim to gain a deeper understanding of how organisms evolve in response to changing environments, ultimately contributing valuable insights to the broader field of evolutionary biology.",
+      "In my Master’s thesis, I delve into genetic mutations and their impact on adaptation in <span className="italic"> S. cerevisiae </span>. My main goal is to unravel the intricacies of the fitness landscape experienced by strains during the adaptation process. To achieve this, I am developing a specialized pipeline to introduce several mutations in yeast cells. By dissecting the genetic components that drive adaptation, I aim to gain a deeper understanding of how organisms evolve in response to changing environments, ultimately contributing valuable insights to the broader field of evolutionary biology.",
   },
  {
-    name: "Resistance development of Pseudomonas aeruginosa over the past half-century",
+    name: "Resistance development of <span className="italic">Pseudomonas aeruginosa</span> over the past half-century",
     date: "May 2023",
     link: "https://github.com/marieverdonck9/portfolio_documents/blob/main/Resistance%20development%20of%20Pseudomonas%20aeruginosa%20over%20the%20past%20half-century.pdf",
     skills: ["Genomic Analysis", "Data Quality Assurance", "Bioinformatics Tools"],
     description:
-      "Investigated antibiotic resistance development in Pseudomonas aeruginosa. Using whole-genome analysis and next-generation sequencing, I compared strains isolated before and after 1940 to study the evolution of antibiotic resistance. The analysis focused on aminoglycoside and fluoroquinolone resistance mechanisms, examining the presence of aminoglycoside-modifying enzymes and mutations in key proteins.",
+      "Investigated antibiotic resistance development in <span className="italic">Pseudomonas aeruginosa</span>. Using whole-genome analysis and next-generation sequencing, I compared strains isolated before and after 1940 to study the evolution of antibiotic resistance. The analysis focused on aminoglycoside and fluoroquinolone resistance mechanisms, examining the presence of aminoglycoside-modifying enzymes and mutations in key proteins.",
   },
   {
     name: "3D analysis of the Fv structure of the IgE SPE-7 in complex with acenaphthenequinone",
@@ -35,7 +35,7 @@ export const projectInfo = [
     description: "Delved into histology as a vital component of my immunology practicals. Primary focus was on the examination of histological sections from pig and chicken lymphoid organs, with an emphasis on understanding how these samples were prepared.",
   },
    {
-    name: "Antimicrobial production in nontoxigenic Clostridium botulinum",
+    name: "Antimicrobial production in nontoxigenic <span className="italic">Clostridium botulinum</span>",
     date: "February - June 2022",
     link: "https://github.com/marieverdonck9/portfolio_documents/blob/main/Article%26Poster_Antimicrobial%20production%20in%20nontoxigenic%20Clostridium%20botulinum%202.pdf",
     skills: [
@@ -44,7 +44,7 @@ export const projectInfo = [
       "Microscopy Techniques",
     ],
     description:
-      "In my Bachelor’s thesis, I explored the antimicrobial potential of three nontoxigenic Clostridium botulinum strains (1985, HA1, and ZO42). I identified bacteriocins, proteinaceous compounds with antibacterial properties, within these strains through whole genome sequence analysis. The study revealed highly potent compounds with distinct modes of action, shedding light on their potential applications as alternative antimicrobial agents.",
+      "In my Bachelor’s thesis, I explored the antimicrobial potential of three nontoxigenic <span className="italic">Clostridium botulinum</span> strains (1985, HA1, and ZO42). I identified bacteriocins, proteinaceous compounds with antibacterial properties, within these strains through whole genome sequence analysis. The study revealed highly potent compounds with distinct modes of action, shedding light on their potential applications as alternative antimicrobial agents.",
   },
   {
     name: "Applying Systems Biology to Give Lone Genes a Meaning",
@@ -71,13 +71,13 @@ export const projectInfo = [
     date: "November 2021",
     link: "https://github.com/marieverdonck9/portfolio_documents/blob/main/Analyzing%20how%20transposon%20Tn3E.F.%20influences%20antibiotic%20resistance%20in%20E.%20coli%20strains%20S17.1%20and%20C2110.pdf",
     skills: ["Genetic Manipulation", "Molecular Biology Techniques"],
-    description: "Used transposons to introduce mutations in E. coli to alter the resistance characteristics of the target plasmid. The goal was to investigate the mutagenic capabilities of transposons and their broader scientific relevance.",
+    description: "Used transposons to introduce mutations in <span className="italic">E. coli</span> to alter the resistance characteristics of the target plasmid. The goal was to investigate the mutagenic capabilities of transposons and their broader scientific relevance.",
   },
  {
     name: "Using the QuickChange method to introduce mutations into algL whilst adding a restriction site for BpmI",
     date: "November 2021",
     link: "https://github.com/marieverdonck9/portfolio_documents/blob/main/Using%20the%20QuickChange%20method%20to%20introduce%20mutations%20into%20algL%20whilst%20adding%20a%20restriction%20site%20for%20BpmI%20.pdf",
     skills: ["Genetic Manipulation", "Molecular Biology Techniques"],
-    description: "Conducted site-directed mutagenesis to introduce specific genetic mutations in Pseudomonas fluorescens. The goal was to investigate genetic modification techniques, contributing to a deeper understanding of precise genetic alterations and their broader applications.",
+    description: "Conducted site-directed mutagenesis to introduce specific genetic mutations in <span className="italic">Pseudomonas fluorescens</span>. The goal was to investigate genetic modification techniques, contributing to a deeper understanding of precise genetic alterations and their broader applications.",
   },
 ];
