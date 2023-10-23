b36d69f4a5e92d17cbccd4a19a92470371aacae3
marieverdonck9 <marieverdonck9@gmail.com>
2023-10-23 21:04:24 +0200

    some improvements

--- src/Experience.js
+++ src/Experience.js
@@ -6,9 +6,11 @@ export default function Experience() {
       <div className="border p-5 rounded-sm mb-4">
         <div className="flex justify-between items-center">
           <h3 className="font-semibold">VIB</h3>
-          <p className="text-slate-400 text-sm font-light">Leuven, BE</p>
+          <p className="text-slate-400 text-sm font-light hidden md:block">
+            Leuven, BE
+          </p>
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col md:items-center md:flex-row justify-between">
           <p className="text-slate-400 text-sm font-medium">Research Student</p>
           <p className="text-slate-400 text-sm font-light">
             Sept 2023 - Present
@@ -24,9 +26,11 @@ export default function Experience() {
       <div className="border p-5 rounded-sm mb-4">
         <div className="flex justify-between items-center">
           <h3 className="font-semibold">IAESTE</h3>
-          <p className="text-slate-400 text-sm font-light">Leuven, BE</p>
+          <p className="text-slate-400 text-sm font-light hidden md:block">
+            Leuven, BE
+          </p>
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col md:items-center md:flex-row justify-between">
           <p className="text-slate-400 text-sm font-medium">Chief Marketing</p>
           <p className="text-slate-400 text-sm font-light">
             Jul 2023 - Present
@@ -40,11 +44,11 @@ export default function Experience() {
       <div className="border p-5 rounded-sm mb-4">
         <div className="flex justify-between items-center">
           <h3 className="font-semibold">Starbucks</h3>
-          <p className="text-slate-400 text-sm font-light">
+          <p className="text-slate-400 text-sm font-light hidden md:block">
             BSCA Charleroi Airport, BE
           </p>
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col md:items-center md:flex-row justify-between">
           <p className="text-slate-400 text-sm font-medium">Barista</p>
           <p className="text-slate-400 text-sm font-light">
             Jul 2019 - Aug 2023
